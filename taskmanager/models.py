94ad4df3332d5d4a385a8baffe9aa8376f389f1f
Cintakins <jacintalerwill@hotmail.com>
2022-05-15 20:33:39 +0000

    added css styling and html for base page and tasks template

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,6 +4,7 @@ class Category(db.Model):
     # schema for primary model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # repr to represent itself in the form of a string
@@ -21,7 +22,7 @@ class Task(db.Model):
 
     def __repr__(self):
         # repr to represent itself in the form of a string
-        return self
+        return f"#{self.id}- Task: {self.task_name}| Urgent: {self.is_urgent}"
 
 
 
